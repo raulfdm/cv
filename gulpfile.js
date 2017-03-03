ed39e4c71911cb1bbacec63024cba487f85a8540
Raul F. de Melo <melo.raulf@gmail.com>
2017-03-03 10:31:41 +0000

    Create flow for build and deploy

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,42 @@
 const gulp = require('gulp'),
-    browser = require('browser-sync');
+    browser = require('browser-sync'),
+    pug = require('gulp-pug'),
+    clean = require('gulp-clean'),
+    ghPages = require('gulp-gh-pages'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename');
 
+gulp.task('build', ['clean'], function () {
+    gulp.start('pug', 'cssmin')
+});
 
-gulp.task('server', function() {
+gulp.task('deploy', function () {
+    return gulp
+        .src('./dist/**/*')
+        .pipe(ghPages());
+});
+
+gulp.task('clean', function () {
+   return gulp
+        .src('dist/')
+        .pipe(clean())
+})
+
+gulp.task('cssmin', function() {
+    return gulp.src('src/**/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('pug', function buildHTML() {
+    return gulp
+        .src('src/*.pug')
+        .pipe(pug({}))
+        .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('server', function () {
 
     //Iniciando o servidor
     browser.init({
@@ -12,5 +46,22 @@ gulp.task('server', function() {
     });
 
     //Escuta de alterações
-    gulp.watch('src/**/*.*').on('change', browser.reload);
+    gulp
+        .watch('src/**/*.*')
+        .on('change', browser.reload);
+})
+
+gulp.task('serverDIST', function () {
+
+    //Iniciando o servidor
+    browser.init({
+        server: {
+            baseDir: 'dist'
+        }
+    });
+
+    //Escuta de alterações
+    gulp
+        .watch('dist/**/*.*')
+        .on('change', browser.reload);
 })
