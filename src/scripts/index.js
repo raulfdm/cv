fcf04f13fd59f7dc302dc20d2972b44f8edf1067
Raul F. de Melo <melo.raulf@gmail.com>
2017-03-05 20:10:45 +0000

    Reorganizating directores and refactor
    - Refactored function combo to apply clousure
    - Changed type of variables: let to const (will not change the content)

---
+++ src/scripts/index.js
@@ -0,0 +1,51 @@
+const combo = (()=> {
+
+    let sequence = [];
+    let backspaceCount = 0;
+    return function(ev) {   
+        if (ev.code === 'Backspace') {
+            if (backspaceCount === 2) {
+                resetSequence()
+                backspaceCount = 0;
+                console.warn(`Resetado: sequence: ${sequence} and backspaceCount: ${backspaceCount}`)
+                return;
+            }
+            backspaceCount++;
+            return;
+        }
+
+        if (sequence.length > 4) 
+            resetSequence();
+        
+        sequence.push(ev.code)
+        let comboSuccess = ["ShiftLeft", "KeyS", "KeyT", "KeyF"];
+        let actualCombo = sequence.slice(sequence.lenght - 4, sequence.lenght);
+
+        if (comboSuccess.toString() === actualCombo.toString()) {
+            changePage();
+        }
+    }
+})()
+
+const changePage = () => {
+    let locationGithub = {
+        origin: window.location.origin,
+        itCV: '/cv/',
+        staffCV: '/cv/staff.html'
+    }
+
+    let actualPage = window.location.pathname;
+
+    if (actualPage === locationGithub.itCV) {
+        window.location.href = locationGithub.origin + locationGithub.staffCV;
+    } else {
+        window.location.href = locationGithub.origin + locationGithub.itCV;
+    }
+}
+const resetSequence = () => {
+    sequence = [];
+}
+
+document
+    .querySelector('html')
+    .addEventListener('keydown', combo)
