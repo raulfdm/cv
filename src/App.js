5b91dfe6ceabe8f120da3292f43c10a69a510c24
Raul Melo <melo.raulf@gmail.com>
2018-02-18 19:12:14 +0100

    Import Headers to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import Headers from './components/Headers';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Headers />
         <h1>oi</h1>
       </div>
     );
