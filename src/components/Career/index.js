c2dbc375de77154d0470692b4d10ff921d29a949
Raul Melo <melo.raulf@gmail.com>
2018-02-18 20:31:13 +0100

    Implement Career Section

---
+++ src/components/Career/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Section, SectionBody, SectionTitle } from '../Section';
+
+export default () => {
+  return (
+    <Section>
+      <SectionTitle title="Carrer Summary" />
+      <SectionBody>
+        Front-end Developer with over 2 years of experience. Perform front-end developer tasks such
+        as creating websites or web applications with css frameworks or from scratch, always based
+        on UI/UX good practices and using or not Javascript Framework. Configure task runner to
+        deploy, test and compiling and automate time-consuming job. Knowledge in development of
+        RESTful API's using NodeJS and data persistence with mongodb or firebase.
+      </SectionBody>
+    </Section>
+  );
+};
