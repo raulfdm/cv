c491e7d7927383a5ddad982ffa4cfbc04bfae1c5
Raul Melo <melo.raulf@gmail.com>
2018-02-18 21:04:44 +0100

    Add General Info (top)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Headers from './components/Headers';
+import Infos from './components/Infos';
 import Career from './components/Career';
-import Skills  from './components/Skills';
+import Skills from './components/Skills';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Headers />
+        <Infos />
         <Career />
         <Skills />
       </div>
---
+++ src/globals/data/general-info.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    label: '+31 06 517 30138',
+    link: '+310651730138',
+    type: 'phone',
+  },
+  {
+    id: 2,
+    label: 'Johan Hofmanstraat 90, Amsterdam',
+    link: 'https://maps.google.com/?q=Johan Hofmanstraat 90, Amsterdam',
+    type: 'regular',
+  },
+  {
+    id: 3,
+    label: 'melo.raulf@gmail.com',
+    link: 'melo.raulf@gmail.com',
+    type: 'email',
+  },
+  {
+    id: 4,
+    label: 'linkedin.com/in/raulfdm',
+    link: 'https://www.linkedin.com/in/raulfdm',
+    type: 'regular',
+  },
+  {
+    id: 5,
+    label: 'github.com/raulfdm',
+    link: 'https://github.com/raulfdm',
+    type: 'regular',
+  },
+];
