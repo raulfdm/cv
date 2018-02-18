0ac1184a0272574334e2f0c4508bddb29e7022cb
Raul Melo <melo.raulf@gmail.com>
2018-02-18 19:54:31 +0100

    Create styled Section component

---
+++ src/components/Section/Section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Section = styled.section`
+  margin-bottom: 2rem;
+  page-break-before: auto;
+`;
+
+export default ({ childre }) => {
+  return <Section>{childre}</Section>;
+};
