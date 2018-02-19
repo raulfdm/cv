2b539fe6f91a1702674050fc2f8497b61b5d9da4
Raul Melo <melo.raulf@gmail.com>
2018-02-20 00:32:35 +0100

    Export data to a separate file

--- src/components/Career/index.js
+++ src/components/Career/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Section, SectionBody, SectionTitle } from '../Section';
+import carrerSummary from '../../globals/data/carrerSummary';
 
 export default () => {
   return (
     <Section>
-      <SectionTitle title="Carrer Summary" />
-      <SectionBody>
-        Front-end Developer with over 2 years of experience. Perform front-end developer tasks such
-        as creating websites or web applications with css frameworks or from scratch, always based
-        on UI/UX good practices and using or not Javascript Framework. Configure task runner to
-        deploy, test and compiling and automate time-consuming job. Knowledge in development of
-        RESTful API's using NodeJS and data persistence with mongodb or firebase.
-      </SectionBody>
+      <SectionTitle title="Career Summary" />
+      <SectionBody>{carrerSummary}</SectionBody>
     </Section>
   );
 };
