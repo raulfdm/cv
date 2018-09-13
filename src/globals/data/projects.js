df3f5c8c4d9b6db6d4323228ff043f67f173ead3
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-09-13 18:16:12 +0200

    Change project description

--- src/globals/data/projects.js
+++ src/globals/data/projects.js
@@ -6,8 +6,8 @@ export default [
     timeInit: moment('01/02/2017', 'DD/MM/YYYY'),
     timeEnd: moment('10/02/2017', 'DD/MM/YYYY'),
     description: [
-      `This project was built in order to make easy to retrieve information about characters on Tibia (MMORPG). So, a POST method is send to the API with a character name in body content, which is used to make a data scraping and return the important data from it.`,
-      `The API was built using NodeJS + ExpressJS and deployed on Heroku. You can consult the documentation and get more details about this project on https://github.com/raulfdm/tibia-api`,
+      `This project was built in order to make easy to retrieve information about characters on Tibia (MMORPG). The user can send a GraphQL query containing the character name and the application will make a data-scrapping on the official game website, organize all those information and send it back.`,
+      `The API was built using NodeJS + ExpressJS + GraphQL and deployed on Heroku. Documentation is available at  https://github.com/raulfdm/tibia-api.`,
     ],
   },
   {
@@ -16,8 +16,8 @@ export default [
     timeInit: moment('01/01/2017', 'DD/MM/YYYY'),
     timeEnd: moment('30/01/2017', 'DD/MM/YYYY'),
     description: [
-      `The main goal of this project was to create a Restful API to be consumed from some client-side or service and to make easier to access all the information about the Brazilian Food Composition Table made by UNICAMP.`,
-      `The API was built using NodeJS + ExpressJS and MongoDB and deployed on Heroku. You can consult the documentation on https://github.com/raulfdm/taco-api`,
+      `The main goal of this project was to create a RESTful API to be consumed for some client-side or services and to make easier to access all the information about the Brazilian Food Composition Table (TACO) made by UNICAMP (University of Campinas).`,
+      `The API was built using NodeJS + ExpressJS and MongoDB and deployed on Heroku. Documentation is available at https://github.com/raulfdm/taco-api.`,
     ],
   },
 ];
