c2cb4df3c9351812601c53c1daa58a539f1d7896
Raul Melo <raulmelo@Rauls-MacBook-Pro.local>
2018-09-16 12:55:02 +0200

    Add firebase config and export useful methods

---
+++ src/globals/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBaFkiQ67qtERkJjxgCiY552_JwdcSdI3Y',
+  authDomain: 'personal-cv-87ac0.firebaseapp.com',
+  databaseURL: 'https://personal-cv-87ac0.firebaseio.com',
+  projectId: 'personal-cv-87ac0',
+  storageBucket: 'personal-cv-87ac0.appspot.com',
+  messagingSenderId: '539911573701',
+};
+
+firebase.initializeApp(config);
+
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+export const cvDatabase = firebase
+  .database()
+  .ref()
+  .child('cv');
+export const firebaseAuth = firebase.auth;
