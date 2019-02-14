abd32ab57cc93630d59dfb7eb938a90889bad755
Raul de Melo <melo.raulf@gmail.com>
2019-02-15 00:08:56 +0100

    Rename function

--- src/utils/date.utils.js
+++ src/utils/date.utils.js
@@ -1,3 +1,3 @@
 import moment from 'moment';
 
-export const formatExperienceDate = date => date && moment(date).format(`MMM'YY`);
+export const formatExperienceDate = date => date && moment(date, 'YYYY-MM-DD').format(`MMM'YY`);
