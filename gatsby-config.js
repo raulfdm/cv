f6921c5ef5a7a476971396be1e93dc0629fa80a9
Raul de Melo <melo.raulf@gmail.com>
2019-02-14 22:25:11 +0100

    Add gatsby-plugin-styled-components dep. e to the config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     'gatsby-plugin-react-helmet',
     {
       resolve: `gatsby-plugin-google-analytics`,
