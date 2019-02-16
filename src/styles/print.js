13deee52fc994fa771f7dca6f59925c892e06536
Raul de Melo <melo.raulf@gmail.com>
2019-02-16 15:08:46 +0100

    Move coding to be refactored related to print mode for a specific file

---
+++ src/styles/print.js
@@ -0,0 +1,24 @@
+/* TODO: Refactor Print
+@media print {
+    html,
+    body {
+      width: 210mm;
+      height: 297mm;
+    }
+    body > section {
+      margin-bottom: 1rem;
+    }
+  }
+
+  @media print {
+    @page {
+      margin: 0;
+      size: A4;
+      margin: 11mm 17mm 17mm 17mm;
+    }
+    body {
+      padding: 1.6cm;
+    }
+  }
+
+*/
