f581907321b60c1bfc0db9dff09b9028a8b7f0f6
Raul de Melo <melo.raulf@gmail.com>
2019-02-16 15:49:27 +0100

    Add sizes to make it easier @medias

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -7,6 +7,15 @@ const theme = {
     sans: `'Lora', sans-serif`,
     serif: `'Raleway', serif`,
   },
+  sizes: {
+    mobileS: '320px',
+    mobileM: '375px',
+    mobileL: '425px',
+    tablet: '768px',
+    laptop: '1024px',
+    laptopL: '1440px',
+    desktop: '2560px',
+  },
 };
 
 export default theme;
