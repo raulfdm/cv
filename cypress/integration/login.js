a24050d0ce7746d8a90919a2505de30ac0c2de2b
Raul de Melo <melo.raulf@gmail.com>
2019-09-01 11:08:22 +0200

    Fix login tests

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,25 +1,32 @@
-it('Login', () => {
-  cy.visit('http://localhost:3000/login');
+describe('Login Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/logout');
+    cy.wait(1000);
+  });
 
-  cy.get('input[type="email"]').type('cypress-test@gmail.com');
-  cy.get('input[type="password"]').type('a5rY&Y2ezMJHaEIZ');
-  cy.get('[data-cy="btn-login"]').click();
-  cy.url().should('eq', 'http://localhost:3000/edit');
-});
+  it('Wrong credentials', () => {
+    cy.visit('http://localhost:3000/login');
 
-it('Wrong credentials', () => {
-  cy.visit('http://localhost:3000/login');
+    cy.get('[data-cy="error-message"]').should('not.exist');
+    cy.get('input[type="email"]').type('cypress-test@gmail.com');
+    cy.get('input[type="password"]').type('cypress-test@gmail.com');
+    cy.get('[data-cy="btn-login"]').click();
+    cy.get('[data-cy="error-message"]').should('exist');
+  });
 
-  cy.get('[data-cy="error-message"]').should('not.exist');
-  cy.get('input[type="email"]').type('cypress-test@gmail.com');
-  cy.get('input[type="password"]').type('cypress-test@gmail.com');
-  cy.get('[data-cy="btn-login"]').click();
-  cy.get('[data-cy="error-message"]').should('exist');
-});
+  it('Button to go home', () => {
+    cy.visit('http://localhost:3000/login');
 
-it('Button to go home', () => {
-  cy.visit('http://localhost:3000/login');
+    cy.get('[data-cy="back-to-home"]').click();
+    cy.url().should('eq', 'http://localhost:3000/');
+  });
 
-  cy.get('[data-cy="back-to-home"]').click();
-  cy.url().should('eq', 'http://localhost:3000/');
+  it('Login', () => {
+    cy.visit('http://localhost:3000/login');
+
+    cy.get('input[type="email"]').type('cypress-test@gmail.com');
+    cy.get('input[type="password"]').type('a5rY&Y2ezMJHaEIZ');
+    cy.get('[data-cy="btn-login"]').click();
+    cy.url().should('eq', 'http://localhost:3000/edit');
+  });
 });
